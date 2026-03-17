9d0620c49b82242dc33e791e033062959596d659
John D'Emic <john.demic@gmail.com>
2026-03-16 21:02:09 -0400

    feat: add --subscriber-id and --organization-ids flags to subscribers create and update

--- cmd/subscribers/update.go
+++ cmd/subscribers/update.go
@@ -10,7 +10,8 @@ import (
 )
 
 func newUpdateCmd() *cobra.Command {
-	var email, firstName, lastName string
+	var email, firstName, lastName, subscriberID string
+	var organizationIDs []string
 
 	c := &cobra.Command{
 		Use:   "update <id>",
@@ -20,7 +21,13 @@ func newUpdateCmd() *cobra.Command {
   revenium subscribers update abc-123 --email new@example.com
 
   # Update multiple fields
-  revenium subscribers update abc-123 --email new@example.com --first-name Jane --last-name Smith`,
+  revenium subscribers update abc-123 --email new@example.com --first-name Jane --last-name Smith
+
+  # Set subscriber ID
+  revenium subscribers update abc-123 --subscriber-id sub-custom-456
+
+  # Update organization membership
+  revenium subscribers update abc-123 --organization-ids org-1,org-2`,
 		Annotations: map[string]string{"mutating": "true"},
 		RunE: func(c *cobra.Command, args []string) error {
 			id := args[0]
@@ -34,6 +41,12 @@ func newUpdateCmd() *cobra.Command {
 			}
 			if c.Flags().Changed("last-name") {
 				updates["lastName"] = lastName
+			}
+			if c.Flags().Changed("subscriber-id") {
+				updates["subscriberId"] = subscriberID
+			}
+			if c.Flags().Changed("organization-ids") {
+				updates["organizationIds"] = organizationIDs
 			}
 
 			if len(updates) == 0 {
@@ -55,6 +68,8 @@ func newUpdateCmd() *cobra.Command {
 	c.Flags().StringVar(&email, "email", "", "Subscriber email address")
 	c.Flags().StringVar(&firstName, "first-name", "", "Subscriber first name")
 	c.Flags().StringVar(&lastName, "last-name", "", "Subscriber last name")
+	c.Flags().StringVar(&subscriberID, "subscriber-id", "", "Custom subscriber identifier")
+	c.Flags().StringSliceVar(&organizationIDs, "organization-ids", nil, "Comma-separated list of organization IDs")
 
 	return c
 }
